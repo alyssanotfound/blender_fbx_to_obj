4680f5900f6e26d9f438f6452f9cd7c98ad2577a
Alyssa Davis <davia317@gmail.com>
2017-03-22 17:41:15 -0400

    converting obj into same fbx folder

--- fbx-to-obj-w-texture.py
+++ fbx-to-obj-w-texture.py
@@ -1,52 +1,30 @@
+# run from command line : blender --background --python fbx-to-obj-w-texture.py
 import bpy 
 import sys
 import os
-
-path_to_fbx_file_folders = "/Users/alyssadavis/Documents/code/AC-Institute/input_fbx_files"
-
-files = folders = 0
-
-argv = sys.argv 
-argv = argv[argv.index("--") + 1:] # get all args after "--"
-
-fbx_in = argv[0] 
-obj_out = argv[1]
-
-# for _, dirnames, filenames in os.walk(path_to_fbx_file_folders):
-#   # ^ this idiom means "we won't be using this value"
-#   	# print(filenames)
-  	
-#     files += len(filenames)
-#     folders += len(dirnames)
-#     print(dirnames)
-#     print(filenames)
-
-# print(folders)
 
 def my_function(directory):
     print("Listing: " + directory)
     for file in os.listdir("."):
     	if file.endswith(".fbx"):
-        	curr_fbx = file
+    		# output_obj_name = file
+        	curr_fbx = os.path.join(directory, file)
         	print(curr_fbx)
     for file in os.listdir("."):
     	if file.endswith(".jpg") or file.endswith(".JPG") or file.endswith(".JPEG") or file.endswith(".jpeg"):
-        	curr_img = file
+        	curr_img = os.path.join(directory, file)
         	print(curr_img)
-        # elif file.endswith(".jpg"):
-        # 	print(file)
-        # else:
-    # print("\t-" + "\n\t-".join(os.listdir("."))) # List current working directory
+    convert(curr_fbx, curr_img, directory)
 
-def convert(i,o):
+def convert(fbx_path,texture_path,output_dir):
 	
-	bpy.ops.import_scene.fbx(filepath=fbx_in, axis_forward='-Z', axis_up='Y')
+	bpy.ops.scene.new(type='EMPTY')
+	bpy.ops.import_scene.fbx(filepath=fbx_path, axis_forward='-Z', axis_up='Y')
 
-	realpath = os.path.expanduser(path_to_fbx_files + 'texture.jpg')
 	try:
-	    img = bpy.data.images.load(realpath)
+	    img = bpy.data.images.load(texture_path)
 	except:
-	    raise NameError("Cannot load image %s" % realpath)
+	    raise NameError("Cannot load image %s" % texture_path)
 
 	mat = bpy.data.materials['Toon']
 	tex = bpy.data.textures.new("Youtube-tex", 'IMAGE')
@@ -54,24 +32,21 @@ def convert(i,o):
 	slot = mat.texture_slots.add()
 	slot.texture = tex
 
-	bpy.ops.export_scene.obj(filepath=obj_out, axis_forward='-Z', axis_up='Y')
-
-	# to call in command line: blender --background --python fbx-to-obj-w-texture.py
-	# -- test.fbx test.obj
-
+	
+	outputpath = output_dir + "/test.obj"
+	bpy.ops.export_scene.obj(filepath=outputpath, axis_forward='-Z', axis_up='Y')
 	return
 
-# Get all the subdirectories of directory_to_check recursively and store them in a list:
-directories = [os.path.abspath(x[0]) for x in os.walk(path_to_fbx_file_folders)]
-directories.remove(os.path.abspath(path_to_fbx_file_folders)) # If you don't want your main directory included
+if __name__ == "__main__":
+	path_to_fbx_file_folders = "/Users/alyssadavis/Documents/code/AC-Institute/input_fbx_files/"
+	# path_to_obj_output_folder = "/Users/alyssadavis/Documents/code/AC-Institute/output_obj_files/"
 
-for i in directories:
-	os.chdir(i)         # Change working Directory
-	my_function(i)      # Run your function
+	files = folders = 0
 
-# if __name__ == "__main__":
-# 	for x in range(0, folders):
-# 		print("folder number")
+	# Get all the subdirectories of directory_to_check recursively and store them in a list:
+	directories = [os.path.abspath(x[0]) for x in os.walk(path_to_fbx_file_folders)]
+	directories.remove(os.path.abspath(path_to_fbx_file_folders)) # If you don't want your main directory included
 
-	# print(folders)
-    # convert(fbx_in, obj_out)
+	for i in directories:
+		os.chdir(i)         # Change working Directory
+		my_function(i)      # Run your function
