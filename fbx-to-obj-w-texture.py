86af8eb70887f3ced051a70787fbcffe1057369e
Alyssa Davis <davia317@gmail.com>
2017-03-22 21:57:18 -0400

    export to fbx

--- fbx-to-obj-w-texture.py
+++ fbx-to-obj-w-texture.py
@@ -3,21 +3,26 @@ import bpy
 import sys
 import os
 
-def my_function(directory):
+def find_fbx(directory):
     print("Listing: " + directory)
     for file in os.listdir("."):
     	if file.endswith(".fbx"):
-    		# output_obj_name = file
-        	curr_fbx = os.path.join(directory, file)
-        	print(curr_fbx)
+        	curr_fbx_file = file
+        	print(curr_fbx_file)
     for file in os.listdir("."):
     	if file.endswith(".jpg") or file.endswith(".JPG") or file.endswith(".JPEG") or file.endswith(".jpeg"):
         	curr_img = os.path.join(directory, file)
         	print(curr_img)
-    convert(curr_fbx, curr_img, directory)
+    # print("BEFORE CONVERTING")
+    # print(curr_fbx_file)	
+    # print(curr_img)
+    convert(curr_fbx_file, curr_img, directory)
 
-def convert(fbx_path,texture_path,output_dir):
-	
+def convert(fbx_p,texture_path,output_dir):
+	fbx_path = os.path.join(output_dir, fbx_p)
+	base, ext = os.path.splitext(fbx_p)
+
+	bpy.ops.scene.delete()
 	bpy.ops.scene.new(type='EMPTY')
 	bpy.ops.import_scene.fbx(filepath=fbx_path, axis_forward='-Z', axis_up='Y')
 
@@ -27,19 +32,21 @@ def convert(fbx_path,texture_path,output_dir):
 	    raise NameError("Cannot load image %s" % texture_path)
 
 	mat = bpy.data.materials['Toon']
-	tex = bpy.data.textures.new("Youtube-tex", 'IMAGE')
+	print("TEX INFO")
+	print(bpy.data.textures)
+	tex = bpy.data.textures.new(base, 'IMAGE')
 	tex.image = img
 	slot = mat.texture_slots.add()
 	slot.texture = tex
-
+	print(slot.texture)
 	
-	outputpath = output_dir + "/test.obj"
-	bpy.ops.export_scene.obj(filepath=outputpath, axis_forward='-Z', axis_up='Y')
+	outputpath = path_to_fbx_output_folder + base + ".fbx"
+	bpy.ops.export_scene.fbx(filepath=outputpath, axis_forward='-Z', axis_up='Y')
 	return
 
 if __name__ == "__main__":
 	path_to_fbx_file_folders = "/Users/alyssadavis/Documents/code/AC-Institute/input_fbx_files/"
-	# path_to_obj_output_folder = "/Users/alyssadavis/Documents/code/AC-Institute/output_obj_files/"
+	path_to_fbx_output_folder = "/Users/alyssadavis/Documents/code/AC-Institute/output_fbx_files/"
 
 	files = folders = 0
 
@@ -49,4 +56,4 @@ if __name__ == "__main__":
 
 	for i in directories:
 		os.chdir(i)         # Change working Directory
-		my_function(i)      # Run your function
+		find_fbx(i)      
